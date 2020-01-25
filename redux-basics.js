cb77ca6a2b5451f77c022ec73ac8bbf3370a5fcb
Craig White <craig.white@and.digital>
2020-01-25 21:40:15 +0000

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,20 @@ const initialState = {
 
 // Reducer (Gets state & action), returns new state
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    // Mutating original state, never do this immutably
+    return {
+      ...state, // Copied values of old state
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    // Mutating original state, never do this immutably
+    return {
+      ...state, // Copied values of old state
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -16,4 +30,7 @@ console.log(store.getState());
 
 // Dispatching an Action
 
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+console.log(store.getState());
 // Subscription
