06041d4440b471ea51aa348a5dad866d0c2566c8
Craig White <craig.white@and.digital>
2020-01-26 14:42:16 +0000

    Added storing state immutably for counter value, with date

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -48,6 +48,15 @@ class Counter extends Component {
         />
         <CounterControl label="Add 10" clicked={this.props.onAddCounter} />
         <CounterControl label="Subtract 15" clicked={this.props.onSubCounter} />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => (
+            <li key={strResult.id} onClick={this.props.onDeleteResult}>
+              {strResult.value}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
@@ -55,7 +64,8 @@ class Counter extends Component {
 // After class, before export ; Get the global state and get me as a property called ctr
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -64,7 +74,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
-    onSubCounter: () => dispatch({ type: "SUB", val: 15 })
+    onSubCounter: () => dispatch({ type: "SUB", val: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResut: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 
