b7eaa283ab85d0fe085a15c80a2075fd5cfb5054
Craig White <craig.white@and.digital>
2020-01-26 21:54:13 +0000

    Outsourced Redux Actions & added remove result

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from "./actions";
+
 const initialState = {
   counter: 0,
   results: []
@@ -5,31 +7,39 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
+    case actionTypes.INCREMENT:
       return {
         ...state,
         counter: state.counter + 1
       };
-    case "DECREMENT":
+    case actionTypes.DECREMENT:
       return {
         ...state,
         counter: state.counter - 1
       };
-    case "ADD":
+    case actionTypes.ADD:
       return {
         ...state,
         counter: state.counter + action.val
       };
-    case "SUB":
+    case actionTypes.SUBTRACT:
       return {
         ...state,
         counter: state.counter - action.val
       };
-    case "STORE_RESULT":
+    case actionTypes.STORE_RESULT:
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
         // Returns an immutable array, one which includes old and new.
+      };
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray
       };
   }
   return state;
